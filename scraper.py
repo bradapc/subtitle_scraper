3708c5f11e7fabcb32855096e195445c6adbaeb5
Brad Cocar <brad.cocar@gmail.com>
2024-02-06 12:37:19 -0800

    Removed old code after realizing error in subtitle site. Reworking with new site.

--- scraper.py
+++ scraper.py
@@ -1,19 +1,4 @@
 import googlesearch
+from urllib.request import Request, urlopen
 import re
 
-search_request = ''
-
-def searchMovieOnIMDB(search_request):
-    search = googlesearch.search(search_request, start=0, stop=10)
-    imdb_item = ''
-    for item in search:
-        if(re.search('.*imdb.com/title/tt.*', item)):
-            imdb_item = item
-            imdb_item = re.sub('.*tt', '', imdb_item)
-            break
-    else:
-        print("Search yielded no imdb links.")
-    if(imdb_item):
-        return imdb_item
-    else:
-        return False
